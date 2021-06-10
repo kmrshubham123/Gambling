30b4d737922d9c1969a93e676d0d5b68b79ca266
kmrshubham123 <kmrshubham123@gmail.com>
2021-06-10 12:59:25 +0530

    [Kumar Shubham]Added:start playGame

---
+++ Gambling/Gambling/Gambler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gambling
+{
+    class Gambler
+    {
+        int Bet= 1;
+        int Amout = 100;
+        public void playGame()
+        {
+            Console.WriteLine("amount is=" + Amout);
+            Console.WriteLine("Bet is "+Bet);
+        }
+
+        
+    }
+}
--- Gambling/Gambling/Program.cs
+++ Gambling/Gambling/Program.cs
@@ -7,6 +7,8 @@ namespace Gambling
         static void Main(string[] args)
         {
             Console.WriteLine("****Weclome to Gambling****");
+            Gambler gambler = new Gambler();
+            gambler.playGame();
         }
     }
 }
